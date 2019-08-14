73e25ab7fe6e76791b3d784ca45966d911a4d3e2
Maria Correa <computer@computer.su>
2019-08-14 04:27:35 -0500

    Pues creo que ya y ahorita no hago mas, hasta luego, igual  procrastine la mayoria del tiempo

---
+++ list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  list.py
+#  
+#  Copyright 2019 Maria Correa <computer@computer>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+class List:
+	def add(self,element):
+		pass
+	def add(self,element,index):
+		pass
+	def clear(self):
+		pass
+	def cointains(self,obj):
+		pass
+	def get(self,index):
+		pass
+	def isEmpty(self):
+		pass
+	def remove(self,index):
+		pass
+	def remove(self,obj):
+		pass
+	def size(self):
+		pass
+	def toArray(self):
+		pass
+		
+
